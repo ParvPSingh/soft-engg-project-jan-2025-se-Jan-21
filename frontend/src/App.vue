0b14efd324e21d78b75ec31ab00b96d3b3aad8ec
om Podey <ompodey@gmail.com>
2025-01-23 15:42:03 +0530

    Initial frontend setup with Vue.js and basic homepage modification
    Initialized the frontend folder using Vue 3 with Babel and Router with vue cli.
    Set up the project structure, including the src/views folder for page-level components.
    views folder: This is where we will store individual pages (e.g., Home, About) to keep the app modular and scalable.
    Modified the homepage to include a "website under construction" logo.
    Development server can be run using cd frontend && npm run serve.

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #eeeeee;
+  
+}
+nav {
+  padding: 30px;
+}
+body {
+  background-color:#2D2D2D;
+}
+nav a {
+  font-weight: bold;
+  color: #4ecca3;
+}
+
+nav a.router-link-exact-active {
+  color: #393e99;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+   <h2>SE Project</h2>
+    <p>Website Under Construction.... Stay Tuned!!!!</p>
+    <img alt="Vue logo" style="height: 50%;" src="../assets/construction.png">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
